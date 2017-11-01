9c96bbb0d5d882bd65f3ef839fec5013552c6e7f
Steel Wagstaff <swagstaff@wisc.edu>
2017-11-01 16:10:13 -0500

    Hypothesis modification plugin initial commit
    Allows users to darken highlight color and resize body element’s right
    margin when annotation pane opens

---
+++ hypo-mods.php
@@ -0,0 +1,199 @@
+<?php
+/*
+ * Plugin Name: Hypothesis Mods
+ * Plugin URI: none
+ * Description: A plugin that allows some modification of the Hypothesis client
+ * Author: Steel Wagstaff
+ * Version: 0.1.0
+ * Author URI: https://steelwagstaff.info
+ * Text Domain:     hypothesis mods
+ * Domain Path:     /languages
+ */
+
+// Exit if called directly.
+defined( 'ABSPATH' ) or die( 'Cannot access pages directly.' );
+
+// Load textdomain
+function hypothesis_load_plugin_textdomain() {
+    load_plugin_textdomain( 'hypothesis', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'hypothesis_load_plugin_textdomain' );
+
+/**
+ * Create settings page (see https://codex.wordpress.org/Creating_Options_Pages)
+ */
+class HypothesisModsSettingsPage {
+	/**
+	 * Holds the values to be used in the fields callbacks
+	 *
+	 * @var array
+	 */
+	private $options;
+
+	/**
+	 * Start up
+	 */
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+		add_action( 'admin_init', array( $this, 'page_init' ) );
+	}
+
+	/**
+	 * Add options page
+	 */
+	public function add_plugin_page() {
+		add_options_page(
+			__( 'Hypothesis Mods Settings', 'HypothesisModsSettingsPage' ),
+			__( 'Hypothesis Mods', 'hypothesis_mods' ),
+			'manage_options',
+			'hypothesis-mods-setting-admin',
+			array( $this, 'create_admin_page' )
+		);
+	}
+
+	/**
+	 * Options page callback
+	 */
+	public function create_admin_page() {
+		// Set class property.
+		$this->options = get_option( 'hypothesis_mods_options' ); ?>
+		<div class="wrap">
+		<form method="post" action="options.php">
+		<?php
+				settings_fields( 'hypothesis_mods_option_group' );
+				do_settings_sections( 'hypothesis-mods-setting-admin' );
+				submit_button();
+			?>
+		</form>
+		</div>
+	<?php }
+
+	/**
+	 * Register and add settings
+	 */
+	public function page_init() {
+		register_setting(
+			'hypothesis_mods_option_group', // Option group.
+			'hypothesis_mods_options', // Option name.
+			array( $this, 'sanitize' ) // Sanitize callback.
+		);
+
+		/**
+		 * Hypothesis Settings
+		 */
+		add_settings_section(
+			'hypothesis_settings_section', // ID.
+			__( 'Hypothesis Settings', 'hypothesis_mods' ), // Title.
+			array( $this, 'settings_section_info' ), // Callback.
+			'hypothesis-mods-setting-admin' // Page.
+		);
+
+		add_settings_field(
+			'darken-highlights',
+			__( 'Darken highlights (to make more visible)', 'hypothesis_mods' ),
+			array( $this, 'darken_highlights_callback' ),
+			'hypothesis-mods-setting-admin',
+			'hypothesis_mods_settings_section'
+		);
+
+    add_settings_field(
+      'hide-annotation-header',
+      __( 'Hide annotation author & date information', 'hypothesis' ),
+      array( $this, 'hide_annotation_header_callback' ),
+      'hypothesis-mods-setting-admin',
+			'hypothesis_mods_settings_section'
+    );
+
+	/**
+   * display Settings
+   */
+  add_settings_section(
+    'hypothesis_display_section', // ID.
+    __( 'Display Settings', 'hypothesis' ), // Title.
+    array( $this, 'display_section_info' ), // Callback.
+    'hypothesis-setting-admin' // Page.
+  );
+
+}
+
+
+	/**
+	 * Sanitize each setting field as needed
+	 *
+	 * @param array $input Contains all settings fields as array keys.
+	 */
+	public function sanitize( $input ) {
+		$new_input = array();
+
+    if ( isset( $input['darken-highlights'] ) ) {
+      $new_input['darken-highlights'] = absint( $input['darken-highlights'] );
+    }
+
+
+    if ( isset( $input['hide-annotation-header'] ) ) {
+			$new_input['hide-annotation-header'] = absint( $input['hide-annotation-header'] );
+		}
+
+		return $new_input;
+	}
+
+  /**
+   * Print the Display Settings section text
+   */
+  public function display_section_info() {
+  ?>
+    <p><?php esc_attr_e( 'Control additional Hypothesis display settings.', 'hypothesis' ); ?></p>
+  <?php }
+
+  /**
+   * Callback for 'darken-highlights'.
+   */
+  public function darken_highlights_callback ( $args ) {
+    $val = isset( $this->options['darken-highlights'] ) ? esc_attr( $this->options['darken-highlights'] ) : 0;
+    printf(
+      '<input type="checkbox" id="darken-highlights" name="hypothesis_mods_options[darken-highlights]" value="1" %s/>',
+      checked( $val, 1, false )
+    );
+  }
+
+  /**
+   * Callback for 'hide-annotation-header'.
+   */
+  public function hide_annotation_header_callback ( $args ) {
+    $val = isset( $this->options['hide-annotation-header'] ) ? esc_attr( $this->options['hide-annotation-header'] ) : 0;
+    printf(
+      '<input type="checkbox" id="hide-annotation-header" name="hypothesis_mods_options[hide-annotation-header]" value="1" %s/>',
+      checked( $val, 1, false )
+    );
+  }
+}
+
+if ( is_admin() ) {
+	$hypothesis_mods_settings_page = new HypothesisModsSettingsPage();
+}
+
+/**
+ * Calls script to resize page upon expansion of Hypothesis annotation pane
+ */
+function adjust_page_width() {
+     wp_enqueue_script( 'hypothesis-demo', plugins_url('/js/demo.js', __FILE__), array('jquery'), false, true );
+  }
+
+  /**
+   * Loads CSS which hides annotation header
+   */
+function enqueue_extra_styles() {
+  // display settings
+  if ( isset( $options['hide-annotation-header'] ) ) {
+    wp_enqueue_style( 'hypo', plugins_url( '/css/hypo.css', __FILE__) );
+  }
+}
+
+/**
+ * Loads CSS which darkens annotations to make more visible
+ */
+function darken_highlights() {
+  if (isset ($options['darken_highlights'] ) ) {
+    wp_enqueue_style( 'darken', plugins_url( 'css/darken.css', __FILE__) );
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Plugin Name ===
+Contributors: SteelWagstaff
+Tags: hypothesis, annotation, comments
+Requires at least: 3.0.1
+Tested up to: 4.7
+Stable tag: 0.1
+License: BSD
+License URI: http://opensource.org/licenses/BSD-2-Clause
+
+== Description ==
+
+A simple plugin that modifies the Hypothesis WP plugin.
+
+== Installation ==
+
+1. Download the zip file from GitHub
+2. Upload to WordPress
+3. Click Activate
+4. Visit your WordPress Settings > Hypothesis Mods page to configure how it works on your site
+
+== Changelog ==
+
+= 0.1 =
+First!
