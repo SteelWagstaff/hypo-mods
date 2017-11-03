ba6f30304e705cecdbbd8a0af0651119fbe3dea8
Steel Wagstaff <swagstaff@wisc.edu>
2017-11-03 14:52:48 -0500

    ideas for resizing

--- js/resize.js
+++ js/resize.js
@@ -1,10 +1,21 @@
 window.hypothesisConfig = function () {
     return {
         onLayoutChange: function(layoutParams) {
+            jQuery('body').css({'padding-right':layoutParams.width});
+        }
+    };
+};
+
+/*
+//possible alternative solution if I can figure out. Still need to account for placement of PB nav buttons :-(
+window.hypothesisConfig = function () {
+    return {
+        onLayoutChange: function(layoutParams) {
             if (body.width() > 768) {
             var width = body.width() - layoutParams.width;
-            jQuery('body').css({'width':$width});
+            jQuery('body').css({'width':width});
           }
         }
     };
 };
+*/
